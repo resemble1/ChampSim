669a24004d90601e63b7cb0dc650fa96414bde99
cmolder <28611108+cmolder@users.noreply.github.com>
2022-01-26 17:22:47 -0600

    Added fairness evaluation code, support for Hawkeye

--- ml_fairness_sim.py
+++ ml_fairness_sim.py
@@ -3,8 +3,11 @@
 import argparse
 import os
 import sys
+from collections import defaultdict
 
-from model import Model
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
 
 default_results_dir = './results'
 default_output_file = './stats.csv'
@@ -15,10 +18,10 @@ default_seed_file = './scripts/seeds.txt'
 
 # No prefetcher
 default_lru_binary = 'bin/hashed_perceptron-no-no-no-no-lru-{n_cores}core'
-default_hawkeye_binary = 'bin/hashed_perceptron-no-no-no-no-???-{n_cores}core' # TODO change
+default_hawkeye_binary = 'bin/hashed_perceptron-no-no-no-no-hawkeye_simple-{n_cores}core'
 
 baseline_names = ['lru', 'hawkeye']
-baseline_fns = ['lru', '???'] # TODO - Obtain Hawkeye binary
+baseline_fns = ['lru', 'hawkeye_simple']
 baseline_binaries = [default_lru_binary, default_hawkeye_binary]
 
 help_str = {
@@ -140,8 +143,9 @@ def build_command():
 
     # Build ChampSims with different replacement policies.
     cores = set([1] + args.cores)
-    if args.target in baseline_fns:
-        for name, fn in zip(baseline_names, baseline_fns):
+
+    for name, fn in zip(baseline_names, baseline_fns):
+        if args.target == 'all' or name in args.target:
             for c in cores:
                 print(f'=== Building {name} ChampSim binary ({fn}), {c} core{"s" if c > 1 else ""} ===')
                 os.system(f'./build_champsim.sh hashed_perceptron no no no no {fn} {c}')
@@ -171,23 +175,6 @@ def run_command():
     
     execution_traces = args.execution_traces
 
-#     # Seed file
-#     seeds = {}
-#     if not os.path.exists(args.seed_file):
-#         print('Seed file "' + args.seed_file + '" does not exist')
-        
-#     else:
-#         with open(args.seed_file, 'r') as f:
-#             for tr in execution_traces:
-#                 for line in f:
-#                     line = line.strip()
-#                     if line.split()[0] in os.path.basename(tr):
-#                         seeds[tr] = line.split()[1]
-#                         break
-#                 else:
-#                     print('Could not find execution trace "{}" in seed file "{}"'.format(tr, args.seed_file))
-#                     seeds[tr] = None
-
     # Generate results directory
     if not os.path.exists(args.results_dir):
         os.makedirs(args.results_dir, exist_ok=True)
@@ -206,18 +193,10 @@ def run_command():
             print(f'Skipping {name} ({binary})')
             continue
         
-        
         if not os.path.exists(binary):
             print(f'{name} ChampSim binary not found, (looked for {binary})')
             exit(-1)
 
-        # if seeds is not None:
-        #     cmd = '{binary} -stat_printing_period {period}000000  -simulation_instructions {sim}000000 -seed {seed} -traces {trace} > {results}/{base_trace}-{base_binary}.txt 2>&1'.format(
-        #         binary=binary, warm=args.num_prefetch_warmup_instructions, sim=args.num_instructions,
-        #         trace=execution_trace, seed=seed, results=args.results_dir,
-        #         period=args.stat_printing_period,
-        #         base_trace=os.path.basename(execution_trace), base_binary=os.path.basename(binary))
-        # else:
         cmd = '{binary} -stat_printing_period {period}000000 -simulation_instructions {sim}000000 -traces {trace} > {results}/{base_traces}-{base_binary}.txt 2>&1'.format(
             binary=binary,
             period=args.stat_printing_period,
@@ -235,85 +214,155 @@ def run_command():
 """
 Eval
 """
-def read_file(path, cache_level='LLC'):
-    expected_keys = ('ipc', 'total_miss', 'useful', 'useless', 'uac_correct', 'iss_prefetches', 'load_miss', 'rfo_miss', 'kilo_inst')
-    data = {}
+def get_traces_per_cpu(path):
+    traces = {}
     with open(path, 'r') as f:
         for line in f:
+            if 'CPU' in line and 'runs' in line:
+                core = int(line.split()[1])
+                # Trace name
+                #traces[core] = os.path.basename(line.split()[-1]).split('.')[0] # TODO check this works for all traces.
+                # File name
+                traces[core] = os.path.basename(line.split()[-1]) 
+                # File name
+                #traces[core] = line.split()[-1] 
+    return traces
+    
+    
+def read_file(path, cache_level='LLC'):
+    #expected_keys = ('trace', 'ipc', 'total_miss', 'useful', 'useless', 'uac_correct', 'iss_prefetches', 'load_miss', 'rfo_miss', 'kilo_inst')
+    expected_keys = ('trace', 'ipc', 'kilo_inst', 'load_miss', 'rfo_miss', 'total_miss')
+    
+    
+    #data = defaultdict(lambda: defaultdict(int)) # Indexed by core -> feature
+    data = defaultdict(dict)
+    
+    # Build trace list
+    data['trace'] = get_traces_per_cpu(path)
+    data['is_homogeneous'] = len(set(data['trace'].values())) == 1
+    
+    # Build other features
+    with open(path, 'r') as f:
+        for line in f:
+            # Finished CPU indicators
             if 'Finished CPU' in line:
-                data['ipc'] = float(line.split()[9])
-                data['kilo_inst'] = int(line.split()[4]) / 1000
+                core = int(line.split()[2])
+                data['ipc'][core] = float(line.split()[9])
+                data['kilo_inst'][core] = int(line.split()[4]) / 1000
+                
+            # Region of interest statistics
+            if 'CPU' in line and line.split()[0] == 'CPU':
+                core = int(line.split()[1])
             if cache_level not in line:
                 continue
             line = line.strip()
             if 'LOAD' in line:
-                data['load_miss'] = int(line.split()[-1])
+                data['load_miss'][core] = int(line.split()[-1])
             elif 'RFO' in line:
-                data['rfo_miss'] = int(line.split()[-1])
+                data['rfo_miss'][core] = int(line.split()[-1])
             elif 'TOTAL' in line:
-                data['total_miss'] = int(line.split()[-1])
-            elif 'USEFUL' in line:
-                data['useful'] = int(line.split()[-6])
-                data['useless'] = int(line.split()[-4])
-                data['uac_correct'] = int(line.split()[-1])
-                data['iss_prefetches'] = int(line.split()[-8])
-
+                data['total_miss'][core] = int(line.split()[-1])
+            # elif 'USEFUL' in line:
+            #     data['useful'][core] = int(line.split()[-6])
+            #     data['useless'][core] = int(line.split()[-4])
+            #     data['uac_correct'][core] = int(line.split()[-1])
+            #     data['iss_prefetches'][core] = int(line.split()[-8])
+    
     if not all(key in data for key in expected_keys):
         return None
 
     return data
 
-def compute_stats(trace, prefetch=None, base=None, baseline_name=None):
-    if prefetch is None:
-        return None
 
-    pf_data = read_file(prefetch)
 
-    iss_prefetches, uac_correct, useful, useless, ipc, load_miss, rfo_miss, kilo_inst = (
-        pf_data['iss_prefetches'], pf_data['uac_correct'], pf_data['useful'], pf_data['useless'], 
-        pf_data['ipc'], pf_data['load_miss'], pf_data['rfo_miss'], pf_data['kilo_inst']
-    )
-    pf_total_miss = load_miss + rfo_miss + useful
-    total_miss = pf_total_miss
+def compute_stats(trace, baseline_name=''):
+    data = read_file(trace)
+    if not data:
+        return ''
+    
+    n_cores = max(data['ipc'].keys()) + 1
+    out = ''
 
-    pf_mpki = (load_miss + rfo_miss) / kilo_inst
-
-    if base is not None:
-        b_data = read_file(base)
-        b_total_miss, b_ipc = b_data['total_miss'], b_data['ipc']
-        b_load_miss = b_data['load_miss']
-        b_rfo_miss = b_data['rfo_miss']
-        b_mpki = b_total_miss / kilo_inst
-
-    if useful + useless == 0:
-        acc = 'N/A'
-    else:
-        acc = str(useful / (useful + useless) * 100)
+    for core in sorted(data['ipc'].keys()):
+        trace, ipc, load_miss, rfo_miss, kilo_inst = (
+            data['trace'][core], data['ipc'][core], data['load_miss'][core], 
+            data['rfo_miss'][core], data['kilo_inst'][core]
+        )
+        is_homogeneous = data['is_homogeneous']
         
-    if total_miss == 0 or base is None:
-        cov = 'N/A'
-        oldcov = 'N/A'
-    else:
-        #cov = str((b_total_miss - load_miss - rfo_miss) / b_total_miss * 100) # formerly str(useful / total_miss * 100)
-        cov = str(((b_load_miss + b_rfo_miss) - (load_miss + rfo_miss)) / (b_load_miss + b_rfo_miss) * 100)
-        oldcov = str(str(useful / total_miss * 100))
         
-    if iss_prefetches == 0:
-        uac = 'N/A'
-    else:
-        uac = str(uac_correct / iss_prefetches * 100)
-        
-    if base is not None:
-        mpki_improv = str((b_mpki - pf_mpki) / b_mpki * 100)
-        ipc_improv = str((ipc - b_ipc) / b_ipc * 100)
-    else:
-        mpki_improv = 'N/A'
-        ipc_improv = 'N/A'
+        mpki = (load_miss + rfo_miss) / kilo_inst
+        out += f'\n{trace},{baseline_name},{core},{n_cores},{is_homogeneous},{mpki},{ipc},{load_miss},{rfo_miss}'
+    
+    return out
 
-    return '{trace},{baseline_name},{acc},{cov},{oldcov},{uac},{mpki},{mpki_improv},{ipc},{ipc_improv}'.format(
-        trace=trace, baseline_name=baseline_name, acc=acc, cov=cov, oldcov=oldcov, uac=uac, mpki=str(pf_mpki),
-        mpki_improv=mpki_improv, ipc=str(ipc), ipc_improv=ipc_improv,
-    )
+
+
+def build_run_statistics(results_dir, output_file):
+    """Build statistics for each run, per-core.
+    """
+    traces = {}
+    for fn in os.listdir(results_dir):
+        trace = fn.split('-hashed_perceptron-')[0]
+        if trace not in traces:
+            traces[trace] = {}
+        for base_fn in baseline_fns:
+            if base_fn == fn.split('-hashed_perceptron-')[1].split('-')[4]:
+                traces[trace][base_fn] = os.path.join(results_dir, fn)
+
+                
+    
+    stats = 'Trace,Baseline,CPU,NumCPUs,HomogeneousMix,MPKI,IPC,LoadMisses,RFOMisses'
+    for trace in tqdm(traces, dynamic_ncols=True, unit='trace'):
+        d = traces[trace]
+
+        for baseline in d:
+            stats += compute_stats(d[baseline], baseline_name=baseline)
+
+    with open(output_file, 'w') as f:
+        print(stats, file=f)
+    
+    
+    
+def build_trace_statistics(run_stats_file):
+    """Build statistics for each trace's fairness,
+    from the run statistics.
+    """
+    run_stats = pd.read_csv(run_stats_file)
+    
+    stats = 'Trace,Baseline,NumCPUs,LowestIPC,HighestIPC,MeanIPC,LowestIPCHomoNorm,HighestIPCHomoNorm,MeanIPCHomoNorm'
+    
+    
+    t = run_stats.groupby('Trace')
+    for trace in run_stats.Trace.unique():
+        b = t.get_group(trace).groupby('Baseline')
+        for baseline in run_stats.Baseline.unique():
+        
+            c = b.get_group(baseline).groupby('NumCPUs')
+            for n_cores in run_stats.NumCPUs.unique():
+                
+                runs = c.get_group(n_cores)
+                
+                
+                homo = runs[runs.HomogeneousMix == True]
+                homo_mean_ipc = homo.IPC.mean() if not homo.empty else np.nan
+                    
+                # Raw min, mean, max IPCs
+                min_ipc, mean_ipc, max_ipc = runs.IPC.min(), runs.IPC.mean(), runs.IPC.max()
+                
+                # Min, mean, max IPCs normalized to homogeneous mix
+                norm_min_ipc = min_ipc - homo_mean_ipc + 1
+                norm_mean_ipc = mean_ipc - homo_mean_ipc + 1
+                norm_max_ipc = max_ipc - homo_mean_ipc + 1
+               
+                
+                
+                
+                print(trace, baseline, n_cores, f'norm_min_ipc={norm_min_ipc}, norm_mean_ipc={norm_mean_ipc}, norm_max_ipc={norm_max_ipc}')
+                #print(runs)
+            
+
+
 
 def eval_command():
     parser = argparse.ArgumentParser(usage=argparse.SUPPRESS, add_help=False)
@@ -321,136 +370,19 @@ def eval_command():
     parser.add_argument('--output-file', default=default_output_file)
 
     args = parser.parse_args(sys.argv[2:])
-
-    traces = {}
-    for fn in os.listdir(args.results_dir):
-        trace = fn.split('-hashed_perceptron-')[0]
-        if trace not in traces:
-            traces[trace] = {}
-        if 'from_file' in fn:
-            traces[trace]['prefetch'] = os.path.join(args.results_dir, fn)
-        else:
-            for base_fn in baseline_fns:
-                if base_fn == fn.split('-hashed_perceptron-')[1].split('-')[3]:
-                    traces[trace][base_fn] = os.path.join(args.results_dir, fn)
-
-    stats = ['Trace,Baseline,Accuracy,Coverage,Coverage_Old,UAC,MPKI,MPKI_Improvement,IPC,IPC_Improvement']
-    for trace in traces:
-        d = traces[trace]
-        print(trace)
-        print(d)
-        
-        baseline_name = 'no' if 'no' in d else 'No Baseline'
-        if 'no' in d:
-            stats.append(compute_stats(trace, d['no'], baseline_name='no'))
-        for pf in d:
-            if pf != 'no':
-                stats.append(compute_stats(trace, d[pf], d['no'] if 'no' in d else None, baseline_name=pf))
-        #if 'no' in d:
-        #    stats.append(compute_stats(trace, d['no'], baseline_name='no'))
-        #    stats.append(compute_stats(trace, d['prefetch'], d['no'], baseline_name='yours'))
-        #else:
-        #    stats.append(compute_stats(trace, d['prefetch'], baseline_name='No Baseline'))
-        #for fn in baseline_fns:
-        #    if fn in d:
-        #        trace_stats = None
-        #        if fn != 'no' and 'no' in d:
-        #            trace_stats = compute_stats(trace, d[fn], d['no'], baseline_name=fn)
-        #        if trace_stats is not None:
-        #            stats.append(trace_stats)
-
-    with open(args.output_file, 'w') as f:
-        print('\n'.join(stats), file=f)
-
-def generate_prefetch_file(path, prefetches):
-    with open(path, 'w') as f:
-        for instr_id, pf_addr in prefetches:
-            print(instr_id, hex(pf_addr)[2:], file=f)
-
-def read_load_trace_data(load_trace, num_prefetch_warmup_instructions):
     
-    def process_line(line):
-        split = line.strip().split(', ')
-        return int(split[0]), int(split[1]), int(split[2], 16), int(split[3], 16), split[4] == '1'
+    #print('=== Building run statistics... ===')
+    #build_run_statistics(args.results_dir, args.output_file)
 
-    train_data = []
-    eval_data = []
-    if load_trace.endswith('.txt'):
-        with open(load_trace, 'r') as f:
-            for i, line in enumerate(f):
-                if line.startswith('***') or line.startswith('Read'):
-                    continue
-                pline = process_line(line)
-                if pline[0] < num_prefetch_warmup_instructions * 1000000:
-                    train_data.append(pline)
-                else:
-                    eval_data.append(pline)
-    elif load_trace.endswith('.txt.xz'):
-        import lzma
-        with lzma.open(load_trace, mode='rt', encoding='utf-8') as f:
-            for i, line in enumerate(f):
-                if line.startswith('***') or line.startswith('Read'):
-                    continue
-                pline = process_line(line)
-                if pline[0] < num_prefetch_warmup_instructions * 1000000:
-                    train_data.append(pline)
-                else:
-                    eval_data.append(pline)
-    else:
-        print('Unsupported load trace file format')
-        exit(-1)
+    print('=== Building trace statistics... ===')
+    build_trace_statistics(args.output_file)
 
-    return train_data, eval_data
 
-def train_command():
-    if len(sys.argv) < 3:
-        print(help_str['train'])
-        exit(-1)
-    #'train': '''usage: {prog} train <load-trace> [--model <model-path>] [--generate <prefetch-file>] [--num-prefetch-warmup-instructions <num-warmup-instructions>]
 
-    parser = argparse.ArgumentParser(usage=argparse.SUPPRESS, add_help=False)
-    parser.add_argument('load_trace', default=None)
-    parser.add_argument('--generate', default=None)
-    parser.add_argument('--model', default=None)
-    parser.add_argument('--num-prefetch-warmup-instructions', type=int, default=default_warmup_instrs)
-
-    args = parser.parse_args(sys.argv[2:])
-
-    train_data, eval_data = read_load_trace_data(args.load_trace, args.num_prefetch_warmup_instructions)
-
-    model = Model()
-    model.train(train_data)
-
-    if args.model is not None:
-        model.save(args.model)
-
-    if args.generate is not None:
-        prefetches = model.generate(eval_data)
-        generate_prefetch_file(args.generate, prefetches)
-
-def generate_command():
-    if len(sys.argv) < 3:
-        print(help_str['generate'])
-        exit(-1)
-
-    #'generate': '''usage: {prog} generate <load-trace> <prefetch-file> [--model <model-path>] [--num-prefetch-warmup-instructions <num-warmup-instructions>]
-    parser = argparse.ArgumentParser(usage=argparse.SUPPRESS, add_help=False)
-    parser.add_argument('load_trace', default=None)
-    parser.add_argument('prefetch_file', default=None)
-    parser.add_argument('--model', default=None, required=True)
-    parser.add_argument('--num-prefetch-warmup-instructions', default=default_warmup_instrs)
-
-    args = parser.parse_args(sys.argv[2:])
-
-    model = Model()
-    model.load(args.model)
-
-    _, data = read_load_trace_data(args.load_trace, args.num_prefetch_warmup_instructions)
-
-    prefetches = model.generate(data)
-
-    generate_prefetch_file(args.prefetch_file, prefetches)
-
+    
+"""
+Help
+"""
 def help_command():
     # If one of the available help strings, print and exit successfully
     if len(sys.argv) > 2 and sys.argv[2] in help_str:
@@ -461,12 +393,15 @@ def help_command():
         print(help_str['help'])
         exit(-1)
 
+    
+
+"""
+Main
+"""
 commands = {
     'build': build_command,
     'run': run_command,
     'eval': eval_command,
-    'train': train_command,
-    'generate': generate_command,
     'help': help_command,
 }
 
